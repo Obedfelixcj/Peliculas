e6af45895383fb3975cf941d2f72729785afdd77
Obed Felix <obed.felix@mrcodigo.com>
2023-12-05 11:40:41 -0700

    cargar api

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// CARGAR PELICULAS DESDE LA API 
+async function loadMovies(searchTerm){
+    const URL = `https://omdbapi.com/?s=${searchTerm}&page=1&apikey=fc1fef96`;
+    const res = await fetch(`${URL}`);
+    const data = await res.json();
+    if(data.Response == "True") displayMovieList(data.Search);
+}
